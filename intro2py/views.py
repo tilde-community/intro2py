661d9bd6ad3191160b3047550b6b95c5f6a0fdf0
Aldrin Navarro <aldrinnavarro16@gmail.com>
2016-03-10 00:34:13 +0800

    Return appropriate http response when username is already taken

--- intro2py/views.py
+++ intro2py/views.py
@@ -1,7 +1,7 @@
 
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.http import HttpResponseForbidden, JsonResponse
+from django.http import HttpResponseForbidden, JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -13,6 +13,6 @@ def register_user(request):
             user = User.objects.create_user(
                 request.POST.get('username'), '1234')
         except IntegrityError:
-            return HttpResponseForbidden('User already registered!')
+            return HttpResponse(status=403, reason='Username already taken!')
         return JsonResponse({'username': user.username, 'pk': user.pk})
     return HttpResponseForbidden()
