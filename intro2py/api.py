5ba62ec03d9ef5cf167496ad789b94337f0e50f8
Aldrin Navarro <aldrinnavarro16@gmail.com>
2016-03-12 03:43:01 +0800

    Added filtering for activities api

--- intro2py/api.py
+++ intro2py/api.py
@@ -1,5 +1,7 @@
 
-from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User
+
+from rest_framework import routers, serializers, viewsets, generics, filters
 
 from questions.models import Question
 from activities.models import Activity
@@ -22,12 +24,36 @@ class QuestionViewSet(viewsets.ModelViewSet):
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Activity
-        fields = ('pk', 'when', 'text', 'kind')
+        fields = ('when', 'text', 'kind')
 
 
 class ActivityViewSet(viewsets.ModelViewSet):
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter)
+    filter_fields = ('kind', 'text', 'when')
+    ordering_fields = '__all__'
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+
+
+class ActivityListView(generics.ListAPIView):
+    queryset = Activity.objects.all()
+    serializer = ActivitySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('pk', 'kind', 'text')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('pk', 'username', 'email')
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ('username', 'email')
+    ordering = ('username',)
 
 
 # Routers provide an easy way of automatically determining the URL conf.
